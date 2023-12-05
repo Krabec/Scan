742095bb0a99d41d3ad25d0918850e383d99e4e1
Krabec <obodeev2015@yandex.ru>
2023-12-05 16:37:15 +0300

    Сверстал авторизацию

--- src/screens/authorization/Autorization.js
+++ src/screens/authorization/Autorization.js
@@ -1,25 +1,27 @@
 import style from './autorization.module.css';
+import google from '../../images/google.svg';
+import facebook from '../../images/facebook.svg';
+import yandex from '../../images/yandex.svg';
 
 function Authorization() {
 	return (
 	  <div className={style.autorization}>
-        <div className={style.buttons}>
-            <button className={style.button}>Войти</button>
-            <button className={style.button}>Зарегистрироваться</button>
+        <div className={style.buttonsHeader}>
+            <button className={style.buttonHeader + " " + style.activeHeaderButton}>Войти</button>
+            <button className={style.buttonHeader}>Зарегистрироваться</button>
         </div>
-        <form>
-            <h3></h3>
-            <label for="username">Логин или номер телефона:</label>
-            <input type="text" id="username"></input>
-            <label for="password">Пароль:</label>
-            <input type="password" id="password"></input>
-            <button>Войти</button>
-            <button>Восстановить пароль</button>
-            <label>Войти через:</label>
-            <div class="social">
-                <div class="go"><i class="fab fa-google"></i>  Google</div>
-                <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
-                <div class="ya"><i class="fab fa-ya"></i>  Яндекс</div>
+        <form className={style.form}>
+            <label className={style.label} for="username">Логин или номер телефона:</label>
+            <input className={style.input} type="text" id="username"></input>
+            <label className={style.label} for="password">Пароль:</label>
+            <input className={style.input} type="password" id="password"></input>
+            <button className={style.buttonEnter} disabled>Войти</button>
+            <button className={style.buttonRecover}>Восстановить пароль</button>
+            <label className={style.label}>Войти через:</label>
+            <div className={style.social}>
+                <button  className={style.bottonVia}><img src={google}></img></button >
+                <button  className={style.bottonVia}><img src={facebook}></img></button >
+                <button  className={style.bottonVia}><img src={yandex}></img></button >
             </div>
         </form>
       </div>
--- src/screens/authorization/PagesAuthorization.js
+++ src/screens/authorization/PagesAuthorization.js
@@ -2,17 +2,24 @@ import Header from "../../shared/header/header";
 import Footer from "../../shared/footer/footer";
 import style from './pagesAuthorization.module.css';
 import Authorization from "./Autorization";
+import characters from '../../images/Characters.svg';
+import castle from '../../images/castle.png'
 
 
 function PagesAuthorization() {
 	return (
 	  <>
 		<Header/>
-		  <main className={style.main + " container"}>
-			<h1>Для оформления подписки на тариф, необходимо авторизоваться.</h1>
+		<main className={style.main + " container"}>
+			<h1 className={style.header}>Для оформления подписки на тариф, необходимо авторизоваться.</h1>
+			<img className={style.imgCastle} src={castle}></img>
 			<Authorization/>
-		  </main>
-		<Footer/>
+			<img className={style.imgCharacters} src={characters}></img>
+		</main>
+		<div className={style.containerFooters}>
+			<Footer/>
+		</div>
+		
 	  </>
 	);
   }

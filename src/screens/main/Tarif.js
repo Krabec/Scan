7977d622f3c77c2da34c20cb86c9ee56d438e9a3
Krabec <obodeev2015@yandex.ru>
2023-12-05 11:16:50 +0300

    Добавлены нижнии карточки главной страницы

--- src/screens/main/Tarif.js
+++ src/screens/main/Tarif.js
@@ -1,15 +1,29 @@
 import style from './tarif.module.css';
 
-function Tarif() {
+function Tarif( {tarif} ) {
 	return (
-	  <div className={style.tarif}>
-        <div className={style.headerDiv}>
-            <div>
-                <p className={style.headerP}></p>
-                <p className={style.description}></p>
+	  <div className={style.tarif} style={tarif.currentTarif ? {border: tarif.border} : {border: "none"}}>
+        <div className={style.headerDiv} style={tarif.tarifStyle} >
+            <div className={style.headerDivText}>
+                <p className={style.headerP}>{tarif.name}</p>
+                <p className={style.descriptionTarif}>{tarif.descriptionTarif}</p>
             </div>
-            <img src={''} alt=''></img>
+            <img src={tarif.img} alt=''></img>
         </div>
+        <div className={style.currentTarif} hidden={!tarif.currentTarif}>Текущий тариф</div>
+        <div className={style.price}>
+          <p className={style.priceDiscount}>{tarif.priceDiscount}</p>
+          <p className={style.priceNoDiscount}>{tarif.priceNoDiscount}</p>
+        </div>
+        <p className={style.descriptionPrice}>{tarif.descriptionPrice}</p>
+        <ul>
+          <h3>В тариф входит</h3>
+          {tarif.includedInTariff.map((elem) => {
+            return <li className={style.includedInTariff}>{elem}</li>
+          })}
+        </ul>
+        <button hidden={!tarif.currentTarif}>Перейти в личный кабинет</button>
+        <button hidden={tarif.currentTarif}>Подробнее</button>
 
       </div>
 	);
--- src/screens/main/Tariffs.js
+++ src/screens/main/Tariffs.js
@@ -2,13 +2,72 @@ import Tarif from './Tarif';
 import style from './tariffs.module.css';
 
 function Tariffs() {
+  const tariffs = [
+    {
+      "name": 'Beginner',
+      "descriptionTarif": "Для небольшого исследования",
+      "img": "",
+      "currentTarif": true,
+      "priceDiscount": "799 ₽",
+      "priceNoDiscount": "1 200 ₽",
+      "descriptionPrice": "или 150 ₽/мес. при рассрочке на 24 мес.",
+      "includedInTariff": [
+        "Безлимитная история запросов",
+        "Безопасная сделка",
+        "Поддержка 24/7"
+      ],
+      tarifStyle: {
+        color: "#000000",
+        backgroundColor: "#FFB64F"
+      },
+      border: "1px solid #FFB64F"
+    },
+    {
+      "name": 'Pro',
+      "descriptionTarif": "Для HR и фрилансеров",
+      "img": "",
+      "currentTarif": false,
+      "priceDiscount": "1 299 ₽",
+      "priceNoDiscount": "2 600 ₽",
+      "descriptionPrice": "или 279 ₽/мес. при рассрочке на 24 мес.",
+      "includedInTariff": [
+        "Все пункты тарифа Beginner",
+        "Экспорт истории",
+        "Рекомендации по приоритетам"
+      ],
+      tarifStyle: {
+        color: "#000000",
+        backgroundColor: "#7CE3E1",
+      },
+      border: "1px solid #7CE3E1"
+    },
+    {
+      "name": 'Business',
+      "descriptionTarif": "Для корпоративных клиентов",
+      "img": "",
+      "currentTarif": false,
+      "priceDiscount": "2 379 ₽",
+      "priceNoDiscount": "3 700 ₽",
+      "descriptionPrice": "",
+      "includedInTariff": [
+        "Все пункты тарифа Pro",
+        "Безлимитное количество запросов",
+        "Приоритетная поддержка"
+      ],
+      tarifStyle: {
+        color: "#FFFFFF",
+        backgroundColor: "#000000",
+      },
+      border: "1px solid #000000"
+    },
+  ]
 	return (
 	  <section className={style.tariffs}>
         <h2>наши тарифы</h2>
         <div className={style.containerTarifs}>
-            <Tarif/>
-            <Tarif/>
-            <Tarif/>
+            <Tarif tarif={tariffs[0]}/>
+            <Tarif tarif={tariffs[1]}/>
+            <Tarif tarif={tariffs[2]}/>
         </div>
       </section>
 	);
